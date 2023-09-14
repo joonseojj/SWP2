3ace1a7284389d259e07169d13161a9033924679
joonseojj <joonseojj@naver.com>
2023-09-14 10:00:37 +0900

    첫번쨰 커밋 : 아두이노 코드 추가

---
+++ 04_example_3.ino
@@ -0,0 +1,24 @@
+#define PIN_LED 13
+unsigned int count,toggle;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN_LED,OUTPUT);
+  Serial.begin(9600);
+  while(!Serial) {
+    ;
+  }
+
+  Serial.println("hello World!");
+  count = toggle = 0;
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(++count);
+  toggle = toggle==1? 0:1;
+  digitalWrite(PIN_LED,toggle);
+  delay(1000);
+}
